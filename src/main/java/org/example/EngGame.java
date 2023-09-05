90a8d1a28d10f17319c3f2f923037cd59d9a2e39
Ilya <kuzovlev92@mail.ru>
2023-09-05 16:32:40 +0300

    dz3oop

---
+++ src/main/java/org/example/EngGame.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EngGame extends AbstractGame{
+    @Override
+    List<String> generateCharList() {
+        List<String> list = new ArrayList<>();
+        for (int i = 'a'; i < 'z'; i++) {
+            list.add(String.valueOf((char)i));
+
+        }
+        return list;
+    }
+}
